04277cae75e7e10c83540bb1d563093dd24b61b5
kodazzi <kodazzi@kodazzi.dlserver.com>
2019-10-15 03:11:09 +0900

    kg向けに改変中4

--- run_squad-2.py
+++ run_squad-2.py
@@ -169,8 +169,8 @@ class KGExample(object):#objectクラスを継承
     self.id = id
     self.nlr_text = nlr_text
     self.nlr_tokens = nlr_tokens
-    self.kgr_text
-    self.kgr_tokens#いらない？
+    self.kgr_text = kgr_text
+    self.kgr_tokens = kgr_tokens#いらない？
 
   def __str__(self):
     return self.__repr__()
@@ -192,21 +192,13 @@ class InputFeatures(object):
 
   def __init__(self,
                unique_id,
-               example_index,
-               doc_span_index,
                tokens,
-               token_to_orig_map,
-               token_is_max_context,
                input_ids,
                input_mask,
                segment_ids
                ):
     self.unique_id = unique_id
-    self.example_index = example_index
-    self.doc_span_index = doc_span_index
     self.tokens = tokens
-    self.token_to_orig_map = token_to_orig_map
-    self.token_is_max_context = token_is_max_context
     self.input_ids = input_ids
     self.input_mask = input_mask
     self.segment_ids = segment_ids
@@ -223,57 +215,56 @@ def read_kg_examples(input_file, is_training):
     return False
 
   examples = []
-  for entry in input_data:
-    for data in entry["data"]:
-
-      #文字列を単語の列に変換する
-      nlr_text = data["nlr"]
-      nlr_tokens = []
-      char_to_word_offset = []
-      prev_is_whitespace = True
-      for c in nlr_text:
-        if is_whitespace(c):
-          prev_is_whitespace = True
+  for data in input_data:
+    
+    #文字列を単語の列に変換する
+    nlr_text = data["nlr"]
+    nlr_tokens = []
+    char_to_word_offset = []
+    prev_is_whitespace = True
+    for c in nlr_text:
+      if is_whitespace(c):
+        prev_is_whitespace = True
+      else:
+        if prev_is_whitespace:
+          nlr_tokens.append(c)
         else:
-          if prev_is_whitespace:
-            nlr_tokens.append(c)
-          else:
-            nlr_tokens[-1] += c
-          prev_is_whitespace = False
-        char_to_word_offset.append(len(nlr_tokens) - 1)
+          nlr_tokens[-1] += c
+        prev_is_whitespace = False
+      char_to_word_offset.append(len(nlr_tokens) - 1)
 
-      #kgrの方も同様の処理
-      id=data["id"]
-      kgr_text = data["kgr"]
-      kgr_tokens = []
-      char_to_word_offset = []
-      prev_is_whitespace = True
-      for c in kgr_text:
-        if is_whitespace(c):
-          prev_is_whitespace = True
+    #kgrの方も同様の処理
+    id=data["id"]
+    kgr_text = data["kgr"]
+    kgr_tokens = []
+    char_to_word_offset = []
+    prev_is_whitespace = True
+    for c in kgr_text:
+      if is_whitespace(c):
+        prev_is_whitespace = True
+      else:
+        if prev_is_whitespace:
+          kgr_tokens.append(c)
         else:
-          if prev_is_whitespace:
-            kgr_tokens.append(c)
-          else:
-            kgr_tokens[-1] += c
-          prev_is_whitespace = False
-        char_to_word_offset.append(len(kgr_tokens) - 1)
-      
-      if is_training:
-        #もしkgrにあたるものがなかったらエラー
-        if (len(data["kgr"]) != 1):
-          raise ValueError(
-              "knowledge graph representation is =0 or <=2.")
+          kgr_tokens[-1] += c
+        prev_is_whitespace = False
+      char_to_word_offset.append(len(kgr_tokens) - 1)
+    
+    if is_training:
+      #もしkgrにあたるものがなかったらエラー
+      if (len(data["kgr"]) != 1):
+        raise ValueError(
+            "knowledge graph representation is =0 or <=2.")
 
-      #ここまでで得たデータをKGExampleクラスのインスタンスに変換する
-      example = KGExample(
-          id=id,
-          nlr_text=nlr_text,
-          nlr_tokens=nlr_tokens,
-          kgr_text=kgr_text,
-          kgr_tokens=kgr_tokens#いらない？
-          )
-      examples.append(example)
+    #ここまでで得たデータをKGExampleクラスのインスタンスに変換する
+    example = KGExample(
+        id=id,
+        nlr_text=nlr_text,
+        nlr_tokens=nlr_tokens,
+        kgr_text=kgr_text,
+        kgr_tokens=kgr_tokens#いらない？
+        )
+    examples.append(example)
 
   return examples
 
@@ -282,193 +273,91 @@ def read_kg_examples(input_file, is_training):
 def convert_examples_to_features(examples,
                                  tokenizer,
                                  max_nlr_length,
-                                 doc_stride,#多分いらない
                                  max_kgr_length,
                                  is_training,
                                  output_fn
                                  ):
   """Loads a data file into a list of `InputBatch`s."""
 
-  unique_id = 1000000000#???
+  unique_id = 1000000000#exampleごとに１ずつ足してく
+  max_seq_length = max_nlr_length + max_kgr_length + 3
 
   #exampleはKGExampleクラスのインスタンス
   for (example_index, example) in enumerate(examples):
     #tokenizerはtokenization.FullTokenizerやtokenization.BasicTokenizerの返り値
     #tokenizerは使わずexample.kgr_tokensでいいと思う
     #kgr_tokens = tokenizer.tokenize(example.kgr_text)
+    nlr_tokens = example.nlr_tokens
     kgr_tokens = example.kgr_tokens
 
     #ナレッジグラフ表現の長さを制限する
+    if len(nlr_tokens) > max_nlr_length:
+      nlr_tokens = nlr_tokens[0:max_nlr_length]
     if len(kgr_tokens) > max_kgr_length:
       kgr_tokens = kgr_tokens[0:max_kgr_length]
 
-    """
-    #何してるのかわからない
-    tok_to_orig_index = []
-    orig_to_tok_index = []
-    all_doc_tokens = []
-    for (i, token) in enumerate(example.doc_tokens):
-      orig_to_tok_index.append(len(all_doc_tokens))
-      sub_tokens = tokenizer.tokenize(token)
-      for sub_token in sub_tokens:
-        tok_to_orig_index.append(i)
-        all_doc_tokens.append(sub_token)
-
-    #何してるのかわからない
-    tok_start_position = None
-    tok_end_position = None
-    if is_training and example.is_impossible:#Qに対するAがないとき
-      tok_start_position = -1
-      tok_end_position = -1
-    if is_training and not example.is_impossible:
-      tok_start_position = orig_to_tok_index[example.start_position]
-      if example.end_position < len(example.doc_tokens) - 1:
-        tok_end_position = orig_to_tok_index[example.end_position + 1] - 1
-      else:
-        tok_end_position = len(all_doc_tokens) - 1
-      (tok_start_position, tok_end_position) = _improve_answer_span(
-          all_doc_tokens, tok_start_position, tok_end_position, tokenizer,
-          example.orig_answer_text)
-    """
-
-    # The -3 accounts for [CLS], [SEP] and [SEP]
-    max_tokens_for_nlr = max_nlr_length - len(kgr_tokens) - 3
-
-    """
-    #この処理はいらないと思う、なぜなら今は入力文として文の途中までとすると意味ないから
-    #一文をまるっとナレッジグラフ化しないと意味ない
-    # We can have documents that are longer than the maximum sequence length.
-    # To deal with this we do a sliding window approach, where we take chunks
-    # of the up to our max length with a stride of `doc_stride`.
-    _DocSpan = collections.namedtuple(  # pylint: disable=invalid-name
-        "DocSpan", ["start", "length"])
-    doc_spans = []
-    start_offset = 0
-    while start_offset < len(all_doc_tokens):
-      length = len(all_doc_tokens) - start_offset
-      if length > max_tokens_for_doc:
-        length = max_tokens_for_doc
-      doc_spans.append(_DocSpan(start=start_offset, length=length))#メインの処理
-      if start_offset + length == len(all_doc_tokens):
-        break
-      start_offset += min(length, doc_stride)
-    """
-
-    #１ループにつきInputFeaturesクラスのインスタンスを１つ定義する
+    #InputFeaturesクラスのインスタンスを１つ定義する
     #doc_spansはdoc_tokensでいいと思うけどわからん
-    for (doc_span_index, doc_span) in enumerate(doc_spans):
-      tokens = []
-      token_to_orig_map = {}
-      token_is_max_context = {}
-      segment_ids = []
-      tokens.append("[CLS]")
+    tokens = []
+    segment_ids = []
+    tokens.append("[CLS]")
+    segment_ids.append(0)
+    for token in nlr_tokens:
+      tokens.append(token)
       segment_ids.append(0)
-      for token in kgr_tokens:
-        tokens.append(token)
-        segment_ids.append(0)
-      tokens.append("[SEP]")
-      segment_ids.append(0)
+    tokens.append("[SEP]")
+    segment_ids.append(0)
 
-      for i in range(doc_span.length):
-        split_token_index = doc_span.start + i
-        token_to_orig_map[len(tokens)] = tok_to_orig_index[split_token_index]
-
-        is_max_context = _check_is_max_context(doc_spans, doc_span_index,#このメソッドの定義は下にある。
-                                               split_token_index)
-        token_is_max_context[len(tokens)] = is_max_context
-        tokens.append(all_doc_tokens[split_token_index])
-        segment_ids.append(1)
-      tokens.append("[SEP]")
+    for token in kgr_tokens:
+      tokens.append(token)
       segment_ids.append(1)
+    tokens.append("[SEP]")
+    segment_ids.append(1)
 
-      input_ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-      # The mask has 1 for real tokens and 0 for padding tokens. Only real
-      # tokens are attended to.
-      input_mask = [1] * len(input_ids)
+    # The mask has 1 for real tokens and 0 for padding tokens. Only real
+    # tokens are attended to.
+    input_mask = [1] * len(input_ids)
 
-      # Zero-pad up to the sequence length.
-      while len(input_ids) < max_seq_length:
-        input_ids.append(0)
-        input_mask.append(0)
-        segment_ids.append(0)
+    # Zero-pad up to the sequence length.
+    while len(input_ids) < max_seq_length:
+      input_ids.append(0)
+      input_mask.append(0)
+      segment_ids.append(0)
 
-      #Falseの場合、例外メッセージを出力する
-      assert len(input_ids) == max_seq_length
-      assert len(input_mask) == max_seq_length
-      assert len(segment_ids) == max_seq_length
+    #Falseの場合、例外メッセージを出力する
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
 
-      """
-      start_position = None
-      end_position = None
-      if is_training and not example.is_impossible:
-        # For training, if our document chunk does not contain an annotation
-        # we throw it out, since there is nothing to predict.
-        doc_start = doc_span.start
-        doc_end = doc_span.start + doc_span.length - 1
-        out_of_span = False
-        if not (tok_start_position >= doc_start and
-                tok_end_position <= doc_end):
-          out_of_span = True
-        if out_of_span:
-          start_position = 0
-          end_position = 0
-        else:
-          doc_offset = len(query_tokens) + 2
-          start_position = tok_start_position - doc_start + doc_offset
-          end_position = tok_end_position - doc_start + doc_offset
+    #ログ出力
+    if example_index < 20:
+      tf.logging.info("*** Example ***")
+      tf.logging.info("unique_id: %s" % (unique_id))
+      tf.logging.info("tokens: %s" % " ".join(
+          [tokenization.printable_text(x) for x in tokens]))
+      tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+      tf.logging.info(
+          "input_mask: %s" % " ".join([str(x) for x in input_mask]))
+      tf.logging.info(
+          "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
 
-      if is_training and example.is_impossible:
-        start_position = 0
-        end_position = 0
-      """
+    #InputFeaturesクラスのインスタンスを定義する
+    feature = InputFeatures(
+        unique_id=unique_id,
+        tokens=tokens,
+        input_ids=input_ids,
+        input_mask=input_mask,
+        segment_ids=segment_ids
+        )
 
-      #ログ出力
-      if example_index < 20:
-        tf.logging.info("*** Example ***")
-        tf.logging.info("unique_id: %s" % (unique_id))
-        tf.logging.info("example_index: %s" % (example_index))
-        tf.logging.info("doc_span_index: %s" % (doc_span_index))
-        tf.logging.info("tokens: %s" % " ".join(
-            [tokenization.printable_text(x) for x in tokens]))
-        tf.logging.info("token_to_orig_map: %s" % " ".join(
-            ["%d:%d" % (x, y) for (x, y) in six.iteritems(token_to_orig_map)]))
-        tf.logging.info("token_is_max_context: %s" % " ".join([
-            "%d:%s" % (x, y) for (x, y) in six.iteritems(token_is_max_context)
-        ]))
-        tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-        tf.logging.info(
-            "input_mask: %s" % " ".join([str(x) for x in input_mask]))
-        tf.logging.info(
-            "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-        if is_training and example.is_impossible:
-          tf.logging.info("impossible example")
-        if is_training and not example.is_impossible:
-          answer_text = " ".join(tokens[start_position:(end_position + 1)])
-          tf.logging.info("start_position: %d" % (start_position))
-          tf.logging.info("end_position: %d" % (end_position))
-          tf.logging.info(
-              "answer: %s" % (tokenization.printable_text(answer_text)))
+    # Run callback
+    output_fn(feature)
 
-      #InputFeaturesクラスのインスタンスを定義する
-      feature = InputFeatures(
-          unique_id=unique_id,
-          example_index=example_index,
-          doc_span_index=doc_span_index,
-          tokens=tokens,
-          token_to_orig_map=token_to_orig_map,
-          token_is_max_context=token_is_max_context,
-          input_ids=input_ids,
-          input_mask=input_mask,
-          segment_ids=segment_ids
-          )
+    unique_id += 1
 
-      # Run callback
-      output_fn(feature)
 
-      unique_id += 1
-
-"""
 def _improve_answer_span(doc_tokens, input_start, input_end, tokenizer,
                          orig_answer_text):
   """Returns tokenized answer spans that better match the annotated answer."""
@@ -504,10 +393,10 @@ def _improve_answer_span(doc_tokens, input_start, input_end, tokenizer,
         return (new_start, new_end)
 
   return (input_start, input_end)
-"""
 
 
-"""
+
+
 def _check_is_max_context(doc_spans, cur_span_index, position):
   """Check if this is the 'max context' doc span for the token."""
 
@@ -543,7 +432,7 @@ def _check_is_max_context(doc_spans, cur_span_index, position):
       best_span_index = span_index
 
   return cur_span_index == best_span_index
-"""
+
 
 
 def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
--- tokenization.py
+++ tokenization.py
@@ -137,7 +137,7 @@ def convert_by_vocab(vocab, items):
   """Converts a sequence of [tokens|ids] using the vocab."""
   output = []
   for item in items:
-    output.append(vocab[item])
+    output.append(vocab.get(item))
   return output
 
 
